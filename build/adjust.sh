0f52da90b5e389826710bf2672d90faaf4eb6280
Chuanren Wu <contact@thedrwu.com>
2020-01-12 15:47:23 +0100

    Passed the basic test samples

--- build/adjust.sh
+++ build/adjust.sh
@@ -3,7 +3,7 @@
 [ ! -d pfb ] && mkdir pfb
 
 for i in cm-super/pfb/sf{rm,sl,ti,cc,ui,sc,ci,bx,bl,bi,xc,oc,rb,bm,tt,st,it,tc,vt,vi,dh,fb,fs,cc,fi}*.pfb ;  do
-    fontforge adjust-stroke.pe "${i#cm-super/pfb/}" 3 6 &
+    fontforge adjust-stroke.pe "${i#cm-super/pfb/}" 1 18 &
 done
 
 wait
